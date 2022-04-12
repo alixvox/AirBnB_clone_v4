20932b8979a2d086b18f87831ce0506e3b63394d
Alex Leeper <alixvox@gmail.com>
2022-04-11 22:10:12 -0500

    Adding task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -22,3 +22,11 @@ $(document).ready(function () {
         }
     });
 });
+
+$.get('http://0.0.0.0:5001/api/v1/status', (data) => {
+  if (data.status == 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
